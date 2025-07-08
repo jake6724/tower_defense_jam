79ccf4b02b24a4282ca90c4806ada39a189e3b22
DanRobertsWrexham <122364727+DanRobertsWrexham@users.noreply.github.com>
2025-07-08 21:20:11 +0100

    Dan Pushed

---
+++ scripts/main_menu.gd
@@ -0,0 +1,29 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$CanvasLayer.show()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
+	#pass
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+	
+
+
+func _on_credits_button_pressed() -> void:
+	$Credits.show()
+	$CanvasLayer.hide()
+
+
+func _on_play_button_2_pressed() -> void:
+	$Credits.hide()
+	$CanvasLayer.show()

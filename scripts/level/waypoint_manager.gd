5e948bb1c21269f9dead9eb5dfa04cfedb804fea
jake6724 <jakealdridge6724@gmail.com>
2025-07-08 17:52:34 -0400

    Add waypoint system

--- scripts/level/waypoint_manager.gd
+++ scripts/level/waypoint_manager.gd
@@ -1,10 +1,14 @@
 class_name WaypointManager
 extends Node2D
 
-var waypoint_path: Array[Vector2]
-
 func _ready():
+	hide()
+
+func get_waypoint_path() -> PackedVector2Array:
+	var waypoint_path: PackedVector2Array
+
 	for child in get_children():
 		waypoint_path.append(GameManager.world_to_grid(child.position)) # Does world to grid because that is the original implementation for manually entered paths
-	
-	GameManager.path_1 = waypoint_path
+
+	print("waypoint path: ", waypoint_path)
+	return waypoint_path

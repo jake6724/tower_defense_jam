6adc2ed655d4edcbde0198e4a26e6b605286959f
jake6724 <jakealdridge6724@gmail.com>
2025-07-09 19:11:16 -0400

    Fix stupid bug

--- scripts/level/level_environment.gd
+++ scripts/level/level_environment.gd
@@ -3,9 +3,5 @@ extends Node2D
 
 # Child References
 @onready var waypoint_manager: WaypointManager = $WaypointManager
-<<<<<<< Updated upstream
 @onready var tilemap: TileMapLayer = $TileMapLayer
 @export var waves: Array[Wave]
-=======
-@export var waves: Array[Wave]
->>>>>>> Stashed changes

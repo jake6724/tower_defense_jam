25b693757c51c8153a46cda9fb64f3e2cef0ad11
jake6724 <jakealdridge6724@gmail.com>
2025-07-10 20:38:58 -0400

    Fix bullet despawning too far from target

--- scripts/towers/bullet.gd
+++ scripts/towers/bullet.gd
@@ -13,7 +13,7 @@ var speed: float = 100
 
 func _physics_process(delta):
 	if target and target.is_alive:
-		if global_position.distance_to(target.global_position) > min_distance:
+		if global_position.distance_to(target.global_position + pos_offset) > min_distance:
 			ap.play("move")
 			global_position = global_position + ((global_position.direction_to(target.global_position + pos_offset)) * speed * delta)
 		
